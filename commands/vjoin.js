76d707add3e698037f713d933659039dd277c277
1Falamingo <44668898+1Falamingo@users.noreply.github.com>
2018-11-28 13:10:45 +0300

    Add files via upload

---
+++ commands/vjoin.js
@@ -0,0 +1,15 @@
+const emojiok = "472324477112418341";
+
+exports.run = (client, message) => {
+    var VC = message.member.voiceChannel;
+    if(!VC) {
+        return message.channel.send("```Вы не находитесь не в одном голосовом канале.```");
+    };
+    var JO = client.voiceConnections;
+    if(!JO) {
+        return message.channel.send("```Бот уже находится в голосовом канале.```");
+    };
+    VC.join();
+    message.channel.send("```Бот подключился к голосовому каналу.```");
+    message.react(emojiok);
+};
